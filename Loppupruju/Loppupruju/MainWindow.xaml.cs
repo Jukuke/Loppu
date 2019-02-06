32cf519b0e4015df66dc5593085f4bbfc5c33574
Juho Kemppainen <juhkuskem@hotmail.com>
2019-02-06 17:57:07 +0200

    Toimiva versio

---
+++ Loppupruju/Loppupruju/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Loppupruju
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void ComboBox_Loaded(object sender, RoutedEventArgs e)
+        {
+            List<string> data = new List<string>();
+            data.Add("Book");
+            data.Add("Computer");
+            data.Add("Chair");
+            data.Add("Mug");
+
+            var comboBox = sender as ComboBox;
+
+            comboBox.ItemsSource = data;
+
+            comboBox.SelectedIndex = 0;
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var comboBox = sender as ComboBox;
+
+            string value = comboBox.SelectedItem as string;
+            this.Title="Selected:"+value;
+        }
+    }
+}
